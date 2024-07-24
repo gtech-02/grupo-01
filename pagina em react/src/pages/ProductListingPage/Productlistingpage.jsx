fb3ea67e0d99316cb842d54d35aa33145d117571
VICTOR DE SOUZA ARAUJO <VICTOR0759@DIGITALCOLLEGE.LOCAL>
2024-07-24 08:55:36 -0300

    .

--- pagina em react/src/pages/ProductListingPage/Productlistingpage.jsx
+++ pagina em react/src/pages/ProductListingPage/Productlistingpage.jsx
@@ -1,10 +1,11 @@
 import { Layout } from "../../Layout";
 import Listagem from "../../components/Cadrs/Cardlist";
-
+import Filtros from "../../components/filtros/filtros";
 export default function Produtoslistingpage(){
     return(
     <>
     <Layout>
+        <Filtros/>
         <Listagem/>
     </Layout>
     </>
