e5e89d602b6e01959b516f83408e54e610ead521
VICTOR DE SOUZA ARAUJO <VICTOR0759@DIGITALCOLLEGE.LOCAL>
2024-07-24 08:36:36 -0300

    ultima att

--- pagina em react/src/App.jsx
+++ pagina em react/src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import Produtoslistingpage from './pages/ProductListingPage/Productlistingpage'
 import Home from './pages/HomePage/home'
-import Cardlist from './components/Cadrs/Cardlist'
-import { Layout } from './Layout'
-import AppView from './pages/ProductViewPage/App'
 
 function App() {
 
   return (
     <>
-    <Layout>
-        <Cardlist/>
-        <AppView />
-    </Layout>
+    <Home/>
     </>
   )
 }
