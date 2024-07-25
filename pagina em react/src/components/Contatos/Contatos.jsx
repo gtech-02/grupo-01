6aed2661188e07eeeb6d2568d39581e41049d581
marcosvinissius <vinissiussilva23@gmail.com>
2024-07-25 11:03:51 -0300

    alteração do footer vini

--- pagina em react/src/components/Contatos/Contatos.jsx
+++ pagina em react/src/components/Contatos/Contatos.jsx
@@ -5,25 +5,25 @@ export default function contatos() {
 
     return ( 
       <Layout>
-        <div className='background'>
+        <div className='backgroundVini'>
       <div class="card text-center card; mx-auto w-100 h-5 " style={{backgroundColor:  '#FFFFFF'}}>
           <div class="card-body; d-flex w-40 h-50" >
-            <div className='button'>
-              <div className='img'>
+            <div className='buttonVini'>
+              <div className='imgVini'>
                 <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/instagram.svg" alt="logo-instagram" />
               </div>
               <p class="card-text"></p>
               <a href="https://www.instagram.com/digitalcollegebr/" class="btn btn-danger">Fale Conosco</a>
               </div>
-            <div className='button'>
-              <div className='img'>
+            <div className='buttonVini'>
+              <div className='imgVini'>
                 <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/facebook.svg" alt="logo-facebook" />
               </div>
               <p class="card-text"></p>
               <a href="https://www.facebook.com/digitalcollegebr" class="btn btn-danger">Fale Conosco</a>
             </div>
-            <div className='button'>
-              <div className='img'>
+            <div className='buttonVini'>
+              <div className='imgVini'>
                 <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/twitter.svg" alt="logo-twitter" />
               </div>
                 <p class="card-text"></p>
