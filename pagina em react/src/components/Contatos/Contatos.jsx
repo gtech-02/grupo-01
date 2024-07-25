7d948fe85cf1e6ce25e29fe3ece4b4897ecd93df
marcosvinissius <vinissiussilva23@gmail.com>
2024-07-24 22:58:41 -0300

    adição de card nos contatos

---
+++ pagina em react/src/components/Contatos/Contatos.jsx
@@ -0,0 +1,11 @@
+import Contatos from "../cabeçario/Header"
+
+export default function contatos() {
+
+    return (<div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+    );
+};
