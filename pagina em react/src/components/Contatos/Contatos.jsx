f4e949d6cd05487f58da7b3bd33dcf9b42d0bbc5
marcosvinissius <vinissiussilva23@gmail.com>
2024-07-24 23:31:59 -0300

    ultima alteração de hoje vini

--- pagina em react/src/components/Contatos/Contatos.jsx
+++ pagina em react/src/components/Contatos/Contatos.jsx
@@ -2,10 +2,17 @@ import Contatos from "../cabeçario/Header"
 
 export default function contatos() {
 
-    return (<div class="card">
-  <div class="card-body">
-    This is some text within a card body.
-  </div>
-</div>
+    return ( <div id="cardFundo" class="card text-center; card text-bg-dark mb-3">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/instagram.svg" alt="" />
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     );
 };
