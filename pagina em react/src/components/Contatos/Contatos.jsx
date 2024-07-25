0df632e05b3c6cbf1187a8fe463fca9a1585acd1
marcosvinissius <vinissiussilva23@gmail.com>
2024-07-25 09:50:00 -0300

    adição da pagina de contatos vini

--- pagina em react/src/components/Contatos/Contatos.jsx
+++ pagina em react/src/components/Contatos/Contatos.jsx
@@ -1,18 +1,37 @@
-import Contatos from "../cabeçario/Header"
+import { Layout } from '../../../src/Layout'
+import './Contatos.css'
 
 export default function contatos() {
 
-    return ( <div id="cardFundo" class="card text-center; card text-bg-dark mb-3">
-        <div class="card-header">
-          Featured
+    return ( 
+      <Layout>
+        <div className='background'>
+      <div class="card text-center card; mx-auto w-100 h-5 " style={{backgroundColor:  '#FFFFFF'}}>
+          <div class="card-body; d-flex w-40 h-50" >
+            <div className='button'>
+              <div className='img'>
+                <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/instagram.svg" alt="logo-instagram" />
+              </div>
+              <p class="card-text"></p>
+              <a href="https://www.instagram.com/digitalcollegebr/" class="btn btn-danger">Fale Conosco</a>
+              </div>
+            <div className='button'>
+              <div className='img'>
+                <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/facebook.svg" alt="logo-facebook" />
+              </div>
+              <p class="card-text"></p>
+              <a href="https://www.facebook.com/digitalcollegebr" class="btn btn-danger">Fale Conosco</a>
+            </div>
+            <div className='button'>
+              <div className='img'>
+                <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/twitter.svg" alt="logo-twitter" />
+              </div>
+                <p class="card-text"></p>
+                <a href="https://x.com/eaicollegers" class="btn btn-danger">Fale Conosco</a>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/instagram.svg" alt="" />
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-      </div>
-    );
+        </Layout>
+      );
 };
