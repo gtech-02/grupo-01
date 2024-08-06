49a5702b1eb9ee4048e291cff74481d4a5fc5e2a
marcosvinissius <vinissiussilva23@gmail.com>
2024-08-06 03:01:04 -0300

    criação da model ProductAlls vini

---
+++ BACKEND/src/models/ProductAll.js
@@ -0,0 +1,46 @@
+const connection = require('../database/connecton');
+const { DataTypes } = require('sequelize');
+
+const ProductAll = connection.define('products shoes', {
+    id: {
+        type: DataTypes.INTEGER
+    },
+    
+    enabled: {
+        type: DataTypes.BOOLEAN,
+    },
+
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    
+    slug: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+
+    use_in_menu: {
+        type: DataTypes.BOOLEAN,
+    },
+
+    stock: {
+        type: DataTypes.INTEGER
+    },
+
+    description: {
+        type: DataTypes.STRING(225),
+    },
+
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+
+    price_witch_discount: {
+        type: DataTypes.false,
+        allowNull: false
+    }
+});
+
+module.exports = ProductAll;
