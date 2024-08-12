65277a004677f109fcae6e877512c239457f05cd
MIRELLA GONCALVES EUFRASIO <MIRELLA6158@DIGITALCOLLEGE.LOCAL>
2024-08-12 11:21:36 -0300

    att do category

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -4,8 +4,12 @@ const app = express();
 app.use(express.json());
 
 const ProductController = require('./controllers/ProductController');
+const CategoryController = require('./controllers/CategoryController');
+
 
 
 app.post('/products', ProductController.create);
+app.get('/v1/category/search', CategoryController,)
+
 
 app.listen(5001);
